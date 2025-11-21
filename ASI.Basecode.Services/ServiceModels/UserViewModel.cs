bcd9bffc4919db88ec54a8503524506d1e4fd149
NARDSKIEE <edbernardanasco@gmail.com>
2025-11-22 01:08:21 +0800

    Revert "Reapply "PASSWORD(WARNING MESSAGE)""
    This reverts commit 91686047d62d5561a4e92d961de9c48e12453dce.

--- ASI.Basecode.Services/ServiceModels/UserViewModel.cs
+++ ASI.Basecode.Services/ServiceModels/UserViewModel.cs
@@ -20,7 +20,6 @@ namespace ASI.Basecode.Services.ServiceModels
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
-        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Confirmation Password is required.")]
