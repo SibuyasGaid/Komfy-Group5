5e7ead24fb37e6e33d7faa950daa46b44635f8aa
NARDSKIEE <edbernardanasco@gmail.com>
2025-11-22 00:11:54 +0800

    PASSWORD(WARNING MESSAGE)
    PASSWORD(WARNING MESSAGE)

--- ASI.Basecode.Services/ServiceModels/UserViewModel.cs
+++ ASI.Basecode.Services/ServiceModels/UserViewModel.cs
@@ -20,6 +20,7 @@ namespace ASI.Basecode.Services.ServiceModels
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Confirmation Password is required.")]
