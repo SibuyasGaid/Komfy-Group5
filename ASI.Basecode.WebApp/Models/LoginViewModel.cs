f32447f65301388cd68ab2d40f76a5064114d57c
Keith Harvey P. Angel <kitkitporcil2@gmail.com>
2025-11-22 01:13:17 +0800

    Minimum Passowrd

--- ASI.Basecode.WebApp/Models/LoginViewModel.cs
+++ ASI.Basecode.WebApp/Models/LoginViewModel.cs
@@ -15,6 +15,7 @@ namespace ASI.Basecode.WebApp.Models
         /// <summary>パスワード</summary>
         [JsonPropertyName("password")]
         [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
     }
 }
